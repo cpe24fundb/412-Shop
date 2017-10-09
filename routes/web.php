9f318400cbe892d348336fe6d4c3c1ced71687c9
darunphop <darunphop.p@gmail.com>
2017-10-09 15:14:47 +0700

    category crud

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'admin'], function () use ($router) {
+    $router->group(['prefix' => 'category'], function () use ($router) {
+        $router->get('/', 'CategoryController@list');
+        $router->get('/{id}/edit', 'CategoryController@editForm');
+        $router->post('/create', 'CategoryController@create');
+        $router->post('/{id}/update', 'CategoryController@update');
+        $router->get('/{id}/delete', 'CategoryController@delete');
+    });
+});
