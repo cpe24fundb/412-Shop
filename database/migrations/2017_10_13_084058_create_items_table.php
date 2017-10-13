7362cbf96038dd54fad792f6f78d463eb2f07d39
apboomap <apboomap@outlook.com>
2017-10-13 15:42:33 +0700

    edit_item_table

---
+++ database/migrations/2017_10_13_084058_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         
+        Schema::create('items', function (Blueprint $table) {
+            $table->integer('sid');
+            $table->integer('mid');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
