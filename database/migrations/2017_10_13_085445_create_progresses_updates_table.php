383ba3065aaf2fcf157f9b5bbdfde2070dc2c8f6
apboomap <apboomap@outlook.com>
2017-10-13 15:55:20 +0700

    rename progress_update_table toprogresses_updates_table

--- database/migrations/2017_10_13_085445_create_progresses_updates_table.php
+++ database/migrations/2017_10_13_085445_create_progresses_updates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProgressUpdateTable extends Migration
+class CreateProgressesUpdatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProgressUpdateTable extends Migration
      */
     public function up()
     {
-        Schema::create('progress_update', function (Blueprint $table) {
+        Schema::create('progresses_updates', function (Blueprint $table) {
             $table->integer('fu_id');
             $tabke->date('date');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateProgressUpdateTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('progress_update');
+        Schema::dropIfExists('progresses_updates');
     }
 }
