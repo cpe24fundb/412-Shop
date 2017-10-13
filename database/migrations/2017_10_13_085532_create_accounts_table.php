4e61c3f6c6bed9ac75178263e83d4746ee7211c4
apboomap <apboomap@outlook.com>
2017-10-13 15:57:44 +0700

    create account_table ane accout_model

---
+++ database/migrations/2017_10_13_085532_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->date('date');
+            $table->integer('income');
+            $table->integer('outcome');
+            $table->integer('today_net');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
