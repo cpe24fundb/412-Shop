41d67c1b4806d1f307655679e335b8c26cd9a37e
apboomap <apboomap@outlook.com>
2017-10-22 00:37:46 +0700

    correct user from belongsToMany to belongsTo

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,38 +1,17 @@
+
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
 
     public function stocks()
     {
-        return $this->belongsToMany('App\Stock');
+        return $this->belongsTo('App\Models\Stock');
     }
 
-    use Authenticatable, Authorizable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
 }
