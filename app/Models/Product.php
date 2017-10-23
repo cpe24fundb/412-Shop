59a278d47dd4dd74242a0ec1cd45bb490560d8c8
darunphop <darunphop.p@gmail.com>
2017-10-23 18:10:12 +0700

    added many-to-many relation of product-notification

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,8 @@ class Product extends Model
         return $this->belongsTo('App\Models\ProductCategory');
     }
 
+    public function notification()
+    {
+        return $this->belongsToMany('App\Models\Notification');
+    }
 }
