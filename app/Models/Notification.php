ea661040a9e978fb417cc43dcaa35cda79ee3bc4
darunphop <darunphop.p@gmail.com>
2017-10-23 17:49:18 +0700

    added many-to-many of notification-subcribe

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-
+    public function subsrcibe(){
+        return $this->belongsToMany('App/Models/Product');
+    }
 
 
 }
