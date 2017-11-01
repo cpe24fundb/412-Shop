d9775e3f8de88b55a27dd7934c8632082117fb18
THEERAPAT SATTHAJARUPONG <theerapat_s@cmu.ac.th>
2017-11-02 00:56:00 +0700

    Created Productseeder and mo dataseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        Schema::disableForeignKeyConstraints();
+        $this->call('ProductSeeder');
+        Schema::enableForeignKeyConstraints();
     }
 }
