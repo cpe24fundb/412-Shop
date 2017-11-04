87253c647160885f6664913ca2877629d47e2acf
darunphop <darunphop.p@gmail.com>
2017-11-04 22:29:08 +0700

    updated all blade to use show_product_list component

--- resources/views/product/allCategory.blade.php
+++ resources/views/product/allCategory.blade.php
@@ -4,21 +4,7 @@
   
   <h1 class="mt-3">Categories</h1>
 
-  <div class="row">  
-    @foreach ($categories as $category)
-    <div class="col-sm-6 col-md-3">
-      <div class="card" style="width: 20rem;">
-        <img class="card-img-top" src="/image/no_image.svg" alt="{{ $category->name }}">
-        <div class="card-body">
-          <h4 class="card-title">{{ $category->name }}</h4>
-          <p class="card-text">{{ $category->description }}</p>
-        </div>
-        <div class="card-body">
-          <a href="#" class="card-link">Detail</a>
-          <a href="#" class="card-link">Follow</a>
-        </div>
-      </div>
-    </div>
-    @endforeach
-  </div>
+  @component('components.show_product_list', ['products' => $categories])
+  @endcomponent
+
 @endsection
--- resources/views/product/popular.blade.php
+++ resources/views/product/popular.blade.php
@@ -4,21 +4,7 @@
   
   <h1 class="mt-3">HOME</h1>
 
-  <div class="row">  
-    @foreach ($billitems as $billitem)
-    <div class="col-sm-6 col-md-4">
-      <div class="card">
-        <img class="card-img-top" src="/image/no_image.svg" alt="{{ $billitem->product->name }}">
-        <div class="card-body">
-          <h4 class="card-title">{{ $billitem->product->name }}</h4>
-          <p class="card-text">{{ $billitem->product->retail_price }}</p>
-        </div>
-        <div class="card-body">
-          <a href="#" class="card-link">Detail</a>
-          <a href="#" class="card-link">Follow</a>
-        </div>
-      </div>
-    </div>
-    @endforeach
-  </div>
+  @component('components.show_product_list', ['products' => $billitems])
+  @endcomponent
+  
 @endsection
