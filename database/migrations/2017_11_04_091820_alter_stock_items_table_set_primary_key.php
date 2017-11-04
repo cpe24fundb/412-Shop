3c53c2ba91ce73429a671e36512fc73bcde9b0e1
C475L4V3 <satethapan.naowasate@gmail.com>
2017-11-04 16:30:55 +0700

    alter stock items table set primary key

---
+++ database/migrations/2017_11_04_091820_alter_stock_items_table_set_primary_key.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterStockItemsTableSetPrimaryKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stock_items', function (Blueprint $table) {
+            $table->primary(['product_id', 'stock_id']);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stock_items', function (Blueprint $table) {
+            $table->dropPrimary('tables_product_id_stock_id_primary');
+            
+        });
+    }
+}
