9c4c39ec561259115f63de04139772d8466d987e
darunphop <darunphop.p@gmail.com>
2017-11-05 22:35:03 +0700

    add spacing betwwn row

--- resources/views/components/products_grid.blade.php
+++ resources/views/components/products_grid.blade.php
@@ -1,6 +1,6 @@
 <div class="row">  
 @foreach ($products as $product)
-<div class="col-sm-6 col-md-4">
+<div class="col-sm-6 col-md-4 mb-4">
   <div class="card">
     <img class="card-img-top" src="@empty($product->image) /image/no_image.svg @endempty {{ $product->image }}" alt="{{ $product->name }}">
     <div class="card-body">
