69dc475de13cfb436cdfe038db5f9c4e2bb261fc
THEERAPAT SATTHAJARUPONG <theerapat_s@cmu.ac.th>
2017-11-05 23:02:46 +0700

    add form method in feedbackform

--- resources/views/feedbackForm.blade.php
+++ resources/views/feedbackForm.blade.php
@@ -6,24 +6,24 @@
             <h1 class="mt-3">Feedback</h1>
         </div>
     </div>
-    <form>
-    <div class="row justify-content-md-center">
-            <div class="col-sm-10 col-md-9">
-                <label for="nameInputField">Name</label>
-                <input type="email" class="form-control" id="nameInputField" aria-describedby="emailHelp" placeholder="Name">
+    <form method="post">
+        <div class="row justify-content-md-center">
+                <div class="col-sm-10 col-md-9">
+                    <label for="nameInputField">Name</label>
+                    <input type="email" class="form-control" id="nameInputField" aria-describedby="emailHelp" placeholder="Name">
 
-                <div class="form-group">
-                    <label for="commentFormField">Comment</label>
-                    <textarea class="form-control" id="commentFormField" rows="4"></textarea>
+                    <div class="form-group">
+                        <label for="commentFormField">Comment</label>
+                        <textarea class="form-control" id="commentFormField" rows="4"></textarea>
+                    </div>
                 </div>
-            </div>
-    </div>
-    <div class="row justify-content-md-center">
-        <div class="col-md-2">
-            <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
-            <button type="reset" class="btn btn-secondary" onclick="clearFields()" value="Reset">Reset</button>
         </div>
-    </div>
+        <div class="row justify-content-md-center">
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
+                <button type="reset" class="btn btn-secondary" onclick="clearFields()" value="Reset">Reset</button>
+            </div>
+        </div>
     </form>
 
 @endsection
