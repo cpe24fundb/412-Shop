366776977af5eec17f451c844e047fa684f4f2b2
THEERAPAT SATTHAJARUPONG <theerapat_s@cmu.ac.th>
2017-11-05 18:57:29 +0700

    fix stock user id

--- database/seeds/StockSeeder.php
+++ database/seeds/StockSeeder.php
@@ -15,7 +15,8 @@ class StockSeeder extends Seeder
             'id' => 1111,
             'created_at' => '2017-11-4 8:00:11',
             'updated_at' => '2017-11-4 8:00:11',
-            'line_total' => 275
+            'line_total' => 275,
+            'user_id' => 631
         ]);
     }
 }
