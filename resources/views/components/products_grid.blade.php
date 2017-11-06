3d07154fc3f2a23a00457f42434b0ba136adc2ab
darunphop <darunphop.p@gmail.com>
2017-11-07 00:26:38 +0700

    added query the left product

--- resources/views/components/products_grid.blade.php
+++ resources/views/components/products_grid.blade.php
@@ -1,5 +1,13 @@
 <div class="row">  
 @foreach ($products as $product)
+
+<?php
+$left = 0;
+$bought = 0;
+$product->stocks->each(function($stock) use (&$left){ $left += $stock->pivot->quantity;});
+$product->bills->each(function($bill) use (&$bought){ $bought += $bill->pivot->quantity; });
+?>
+
 <div class="col-sm-6 col-md-4 mb-4">
   <div class="card">
     <img class="card-img-top" src="@empty($product->image) /image/no_image.svg @endempty {{ $product->image }}" alt="{{ $product->name }}">
@@ -8,7 +16,15 @@
       <p class="card-text">{{ $product->retail_price }}</p>
     </div>
     <div class="card-footer">
-      <a href="/product/{{ $product->id }}" class="card-link">Detail</a>
+      <div class="card-group">
+        <div class="card">
+          <a href="/product/{{ $product->id }}" class="btn card-link">Detail</a>
+        </div>
+        <div class="card">
+          <p>Left on stock : {{ $left - $bought }}</p>
+        </div>
+      </div>
+      
     </div>
   </div>
 </div>
