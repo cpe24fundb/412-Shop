461c9d2f4feaee35fb560d256ac160059150c230
darunphop <darunphop.p@gmail.com>
2017-11-07 02:47:25 +0700

    final touch

--- resources/views/components/products_grid.blade.php
+++ resources/views/components/products_grid.blade.php
@@ -1,8 +1,5 @@
 <div class="row">  
 @foreach ($products as $product)
-
-<?php $balance = $product->balance(); ?>
-
 <div class="col-sm-6 col-md-4 mb-4">
   <div class="card">
     <img class="card-img-top" src="@empty($product->image) /image/no_image.svg @endempty {{ $product->image }}" alt="{{ $product->name }}">
@@ -16,7 +13,7 @@
           <a href="/product/{{ $product->id }}" class="btn card-link">Detail</a>
         </div>
         <div class="card">
-          <p style="text-align:center; margin-bottom: 0;" class="btn"> @if($balance) On Stock : {{ $balance }} @else <a class="text-danger">OUT OF STOCK</a> @endif </p>
+          <p style="text-align:center; margin-bottom: 0;" class="btn"> @if($product->balance()) On Stock : {{ $product->balance() }} @else <a class="text-danger">OUT OF STOCK</a> @endif </p>
         </div>
       </div>
       
