42f1f532ecdea7a276f620f0e8eb52b7af1ab9fc
Chawasit Tengtrairat <chawasit555@gmail.com>
2017-11-06 15:21:22 +0700

    remove product relation from notification model

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    public function products(){
-        return $this->belongsToMany('App\Models\Product', 'subscribes');
-    }
+
 }
